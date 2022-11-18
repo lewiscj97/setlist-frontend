72202354856104e2634d0c4434752f3d4c20a814
Lewis Jones <lewis.jones@engineering.digital.dwp.gov.uk>
2022-11-18 14:40:55 +0000

    feat(search): display setlist results and styles with select button

--- src/assets/scripts/artistSearch.js
+++ src/assets/scripts/artistSearch.js
@@ -14,6 +14,9 @@ async function searchArtist() {
     const responseJson = await response.json();
     if (responseJson.length > 0) {
       const responseContainer = document.getElementById('artist-results');
+      const setlistContainer = document.getElementById('setlist-results');
+      responseContainer.innerHTML = '';
+      setlistContainer.innerHTML = '';
       const headingElem = document.createElement('h3');
       headingElem.innerText = 'Search Results:';
       responseContainer.appendChild(headingElem);
@@ -30,8 +33,40 @@ async function searchArtist() {
         responseContainer.appendChild(elem);
       });
       artists.forEach((artistElem) => {
-        artistElem.addEventListener('click', () => {
-          console.log(artistElem.id);
+        artistElem.addEventListener('click', async () => {
+          const artistSetlists = await fetch(`http://localhost:3000/artist/${artistElem.id}/setlists`);
+          const artistSetlistsJson = await artistSetlists.json();
+          setlistContainer.innerHTML = '';
+          const setlistsHeading = document.createElement('h3');
+          setlistsHeading.innerText = 'Setlist Results:';
+          artistSetlistsJson.forEach((setlist) => {
+            const setlistDiv = document.createElement('div');
+            setlistDiv.classList.add('artist-setlist-container');
+            const detailsContainer = document.createElement('div');
+            const date = setlist.eventDate;
+            const venue = setlist.venueName;
+            const tour = setlist?.tourName;
+            const dateElem = document.createElement('p');
+            dateElem.innerText = date;
+            const venueElem = document.createElement('p');
+            venueElem.innerText = venue;
+            detailsContainer.appendChild(dateElem);
+            detailsContainer.appendChild(venueElem);
+            if (tour) {
+              const tourElem = document.createElement('p');
+              tourElem.innerText = tour;
+              detailsContainer.appendChild(tourElem);
+            }
+            setlistDiv.appendChild(detailsContainer);
+            const selectContainer = document.createElement('div');
+            const selectButton = document.createElement('button');
+            selectButton.id = setlist.id;
+            selectButton.innerText = 'Select';
+            selectButton.classList.add('btn', 'btn-primary');
+            selectContainer.appendChild(selectButton);
+            setlistDiv.appendChild(selectContainer);
+            setlistContainer.appendChild(setlistDiv);
+          });
         });
       });
     }
